036d62af53b65d31334fd9515180d024d817f238
ashley <ashparore@gmail.com>
2021-09-28 21:11:56 -0500

    Registro_venta_html_css_js

---
+++ registro_de_ventas/js/funciones.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //activar campos para registrar cliente
+    $('.btn_new_cliente').click(function(e){
+        e.preventDefault();
+        $('#nom_cliente').removeAttr('disabled');
+        $('#tel_cliente').removeAttr('disabled');
+        $('#dir_cliente').removeAttr('disabled');
+
+        $('div_registro_cliente').slideDown();
+    });
+});//end ready
